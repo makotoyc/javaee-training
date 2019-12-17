fa89e719ed112036bf937a31014438daf5e0ef16
makotoyc <makotoyc@gmail.com>
2019-12-18 04:11:02 +0900

    2-2-2

--- training_sample/src/main/java/training/bean/BookBean.java
+++ training_sample/src/main/java/training/bean/BookBean.java
@@ -3,7 +3,9 @@ package training.bean;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.interceptor.Interceptors;
 
+import training.interceptor.BookInterceptor;
 import training.service.BookService;
 
 @ManagedBean
@@ -14,7 +16,7 @@ public class BookBean {
 	private BookService bookService;
 
 	public void exec() {
-		// day2-1-1
+		// FIXME: day2-1-1
 		this.bookService.findAll().forEach(System.out::println);
 	}
 }
