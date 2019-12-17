ef614e4ff46f63c257acf1cbdcddc3be7814a801
makotoyc <makotoyc@gmail.com>
2019-12-17 21:08:12 +0900

    不要記述を削除

--- training_sample/src/main/java/training/bean/BookEntryBean.java
+++ training_sample/src/main/java/training/bean/BookEntryBean.java
@@ -1,13 +1,7 @@
 package training.bean;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.bean.ManagedBean;
-
-import training.bean.Book;
 
 @ManagedBean
 @RequestScoped
@@ -15,22 +9,10 @@ public class BookEntryBean {
 
 	// 登録用フィールド
 	private Book book;
-	private List<Book> books;
-
-	@PostConstruct
-	public void init() {
-		this.book = new Book();
-		// booksの初期化
-		// FIXME: データの取得は
-		this.books = new ArrayList<>();
-		this.books.add(new Book("isbn", "タイタニック", 900));
-		this.books.add(new Book("isbn2", "フローズン", 1200));
-	}
 
 	public String entry() {
 		// FIXME データの登録は未修
 		System.out.println(this.book);
-		this.books.add(this.book);
 		return "bookEntryResult";
 	}
 
@@ -40,13 +22,5 @@ public class BookEntryBean {
 
 	public void setBook(Book book) {
 		this.book = book;
-	}
-
-	public List<Book> getBooks() {
-		return books;
-	}
-
-	public void setBooks(List<Book> books) {
-		this.books = books;
 	}
 }
